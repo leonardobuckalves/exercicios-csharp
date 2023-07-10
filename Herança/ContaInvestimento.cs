f01c007847a2d8680583b32c597d2b879b6e7127
leonardobalves <leonardobuckalves@gmail.com>
2023-07-10 07:05:38 -0300

    exercício herança bug contaInvestimento -método sacar agora funciona corretamente

--- Herança/ContaInvestimento.cs
+++ Herança/ContaInvestimento.cs
@@ -19,7 +19,7 @@ class ContaInvestimento : Conta
     {
         if (Saldo >= valor)
         {
-            Saldo -= valor - CalculaImposto(Imposto);
+            Saldo = Saldo - valor - CalculaImposto(Imposto);
             return Saldo;
         }
         else
