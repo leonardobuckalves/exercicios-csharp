b68e6f16ffc4df4bf97cc3c6b007f514aba9b52b
leonardobalves <leonardobuckalves@gmail.com>
2023-08-21 15:42:41 -0300

    Exercício Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿ClasseGenerica<int> objInt = new ClasseGenerica<int>();
+
+objInt.Adicionar(10);
+objInt.Adicionar(20);
+objInt.Adicionar(30);
+objInt.Adicionar(40);
+objInt.Adicionar(50);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(objInt[i]);
+}
+
+
+public class ClasseGenerica<T>
+{
+    //array com 5 elementos
+    T[] obj = new T[5];
+    int indice = 0;
+
+    //método adicionar
+    public void Adicionar(T item)
+    {
+        if(indice < 5)
+        {
+            obj[indice] = item;
+        }
+        indice++;
+    }
+
+    //indexador
+    public T this[int index]
+    {
+        get { return obj[index]; }
+        set { obj[index] = value; }
+    }
+}
---
+++ Generics/readme.md
@@ -0,0 +1,7 @@
+﻿# Generics - Exercício
+
+Criar uma classe genérica que deverá ser implementada de acordo com os seguintes requisitos:
+
+- Ser um Array genérico de uma dimensão de tamanho igual a 5.
+- Possuir um método Adicionar() responsável por adicionar qualquer tipo de objetos no array.
+- Possuir uma propriedade Indexer como uma implementação para uma instrução foreach. (Usar o conceito de indexador)
