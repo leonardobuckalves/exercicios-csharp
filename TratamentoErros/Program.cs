0133b5155848cc9f5eaca35e6c764d53f4682204
leonardobuckalves <leonardobuckalves@gmail.com>
2023-10-26 13:34:18 -0300

    Tratamento de erros
    bloco try-catch-finally

---
+++ TratamentoErros/Program.cs
@@ -0,0 +1,101 @@
+﻿//// Exercício 1
+//string resposta = null;
+
+//try
+//{
+//    Console.WriteLine("Escreva um valor: ");
+//    resposta = Console.ReadLine();
+
+//    if (resposta == null)
+//    {
+//        throw new ArgumentNullException("resposta", "A entrada não pode ser nula.");
+//    }
+//    else if (string.IsNullOrWhiteSpace(resposta))
+//    {
+//        throw new ArgumentException("A entrada não pode estar vazia.", "entrada");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Entrada: " + resposta); // Exibe a entrada
+//    }
+//}
+//catch (ArgumentNullException ex)
+//{
+//    Console.WriteLine("Erro: " + ex.Message);
+//}
+//catch (ArgumentException ex)
+//{
+//    Console.WriteLine("Erro: " + ex.Message);
+//}
+//finally
+//{
+//    // O bloco finally será executado independentemente de ter ocorrido uma exceção ou não
+//    Console.WriteLine("Fim do programa.");
+
+//}
+
+// Exercício 2
+//int idade = 0;
+//string nome = null;
+//try
+//{
+//    Console.WriteLine("Informe a sua idade");
+//    idade = Convert.ToInt32(Console.ReadLine());
+//    if (idade < 0)
+//    {
+//        throw new ArgumentNullException("A idade não pode ser negativa.");
+//    }
+//    if (idade == 0)
+//    {
+//        throw new NotImplementedException("A idade ainda não foi definida.");
+//    }
+//    Console.WriteLine("Informe o seu nome");
+//    nome = Console.ReadLine();
+//    if (string.IsNullOrEmpty(nome))
+//    {
+//        throw new NullReferenceException("O nome não pode ser nulo nem vazio");
+//    }
+//    Console.WriteLine($"Nome: {nome}, Idade: {idade}");
+//}
+//catch (ArgumentException e)
+//{
+//    Console.WriteLine("Erro de argumento: " + e.Message);
+//}
+//catch (NotImplementedException e)
+//{
+//    Console.WriteLine("Erro de implementação: " + e.Message);
+//}
+//catch (NullReferenceException e)
+//{
+//    Console.WriteLine("Erro de referência nula: " + e.Message);
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine("Erro inesperado: " + e.Message);
+//}
+// Exercício 3
+int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+Console.WriteLine("Números no array:");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"[{i}] - {numeros[i]}");
+}
+try
+{
+    Console.Write("Informe um índice para obter o valor correspondente: ");
+    int indice = int.Parse(Console.ReadLine());
+    int valor = numeros[indice];
+    Console.WriteLine($"O valor no índice {indice} é: {valor}");
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine($"Erro: O índice está fora do intervalo válido.");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine($"Erro: A entrada não pode ser nula.");
+}
+finally
+{
+    Console.WriteLine("Fim do programa.");
+}
---
+++ TratamentoErros/README.md
@@ -0,0 +1,35 @@
+﻿# Exercício 1
+
+Escreva um program onde o usuário é solicitado a informar um valor via teclado e armazenar o valor na
+variável entrada do tipo string onde tem que verificar 3 condições diferentes:
+
+- Se a entrada é nula, uma exceção ArgumentNullException é lançada com a mensagem "A entrada não pode ser
+nula." (Verifique a diferença entre ArgumentException e ArgumentNullException)
+-Se a entrada está vazia, uma exceção ArgumentException é lançada com a mensagem "A entrada não pode estar
+vazia."
+-Se a entrada passar nas verificações anteriores, a entrada é exibida na tela.
+Realize o tratamento de erro para essas condições usando o bloco try-cath-finally
+
+# Exercício 2
+
+Escreva um programa que solicite ao usuário a informação da idade e do nome via teclado que deverão ser
+armazenados nas variáveis idade do tipo int e nome do tipo string.
+A seguir realize o tratamento de erro e lançando as exceções considerando as seguintes condições:
+
+- Se a idade é negativa, uma exceção ArgumentException é lançada com a mensagem "A idade não pode ser
+negativa."
+- Se a idade é zero, uma exceção NotImplementedException é lançada com a mensagem "A idade ainda não foi
+definida."
+- Se o nome é nulo ou vazio, uma exceção NullReferenceException é lançada com a mensagem "O nome não pode
+ser nulo nem vazio"
+Nota: No item c use a expressão string.IsNullOrEmpty(nome)para verificar se o nome é null ou
+vazio.
+
+# Exercício 3
+
+Dado um array de inteiros expresso da seguinte forma
+int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+Exiba na janela do console os numeros do array e solicite via teclado ao usuário para informar o valor de um índice
+do array para obter o seu respectivo valor.
+Tratamento de erros
+Realize o tratamento de exceções filtrando as exceções IndexOutOfRangeException e ArgumentNullException
